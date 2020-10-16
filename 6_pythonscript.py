675ec39ec22c2048f2026074ca13077e55bc037a
Joshua Corrick <joshua.corrick@live.com>
2020-10-15 20:51:25 -0400

    PowerPoint

--- 6_pythonscript.py
+++ 6_pythonscript.py
@@ -1,15 +1,16 @@
 #! /home/josh/.pyenv/versions/3.9.0/bin/python
-#import packages for web, and regex
 
 """Prints a sting for example"""
 print('This is a Script')
 
+#import packages for web, and regex
 import urllib
 import urllib.request
 import re
 
 site_to_check = ['https://status.twitterstat.us/']
 
+#create a function to check the status
 def check_status(url):
     page_content = urllib.request.urlopen(url=url).readlines()
     results = re.findall('Service Disruption',str(page_content))
@@ -19,9 +20,3 @@ def check_status(url):
 
 for site in site_to_check:
     check_status(url=site)
-
-
-
-
-
-
